3cbe3db96fddcb29802ec04ffcd413d46e74c52c
chenmingliang <1825720338@qq.com>
2025-04-03 14:27:21 +0800

    feat: 修改配置

--- src/pages/music/index.vue
+++ src/pages/music/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>音乐</div>
+  <view
+    class="bg-white overflow-hidden pt-2 px-4"
+    :style="{ marginTop: safeAreaInsets?.top + 'px' }"
+  >
+    <view>音乐</view>
+  </view>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 获取屏幕边界到安全区域距离
+const { safeAreaInsets } = uni.getSystemInfoSync()
+</script>
 
 <style scoped lang="scss"></style>
